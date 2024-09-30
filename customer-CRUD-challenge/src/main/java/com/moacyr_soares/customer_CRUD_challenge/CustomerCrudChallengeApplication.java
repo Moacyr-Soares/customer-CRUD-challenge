f4ae699aa265c7e31bcdf422f53faee04e3ec078
Moacyr-Soares <soares.moacyr.r@gmail>
2024-09-30 16:35:19 -0300

    Starting customer-CRUD-challenge

---
+++ customer-CRUD-challenge/src/main/java/com/moacyr_soares/customer_CRUD_challenge/CustomerCrudChallengeApplication.java
@@ -0,0 +1,14 @@
+package com.moacyr_soares.customer_CRUD_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerCrudChallengeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CustomerCrudChallengeApplication.class, args);
+	}
+
+}
