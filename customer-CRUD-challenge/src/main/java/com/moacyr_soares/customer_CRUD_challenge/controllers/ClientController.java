971b63f28da5310ccf65d476279078d6c8b861b9
Moacyr-Soares <soares.moacyr.r@gmail>
2024-10-02 12:33:48 -0300

    Seeding

--- customer-CRUD-challenge/src/main/java/com/moacyr_soares/customer_CRUD_challenge/controllers/ClientController.java
+++ customer-CRUD-challenge/src/main/java/com/moacyr_soares/customer_CRUD_challenge/controllers/ClientController.java
@@ -1,9 +1,14 @@
 package com.moacyr_soares.customer_CRUD_challenge.controllers;
 
 
+import com.moacyr_soares.customer_CRUD_challenge.dto.ClientDTO;
 import com.moacyr_soares.customer_CRUD_challenge.entities.Client;
 import com.moacyr_soares.customer_CRUD_challenge.repositories.ClientRepository;
+import com.moacyr_soares.customer_CRUD_challenge.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,11 @@ import java.util.Optional;
 @RequestMapping(value = "/clients")
 public class ClientController {
 
+    @Autowired
+    private ClientService service;
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
 }
---
+++ customer-CRUD-challenge/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Moacyr Soares', '22296496872', 7000.00, '1988-07-23', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Dionatan Soares', '22298700014', 10000.00, '1990-08-07', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Matheus Oliveira', '55598977737', 20000.00, '2000-03-03', 0);
